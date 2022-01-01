a0cd4d9ed7920804f72a09e0d423a69461bc800f
Antonio Gil <antonioluisgil@gmail.com>
2022-01-01 13:50:19 +0100

    Add: Create project structure

---
+++ README.md
@@ -0,0 +1,8 @@
+<p align="center">
+  <a href="https://fastdaily.netlify.app/" target="__blank">
+    <img src="./src/assets/logo.png" width="325">
+  </a>
+</p>
+
+Fast Daily helps you control the timing of daily meetings. Visualize for each team member how long their turn takes.
+
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto mt-8">
+    <header class="flex justify-center">
+      <img
+        class="aspect-auto text-center"
+        alt="Fast Daily logo"
+        src="./assets/logo.svg"
+        width="325"
+      />
+    </header>
+  </div>
+</template>
